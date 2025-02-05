519b5ae8d2e591c4d9912fb014b423e6e881b2e1
hassan kourani <89477138+HassanKourani@users.noreply.github.com>
2025-02-05 23:28:10 +0200

    fix yarn build

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import type { InitOptions } from "i18next";
 
 import enTranslations from "../locales/en/landing.json";
 import arTranslations from "../locales/ar/landing.json";
@@ -13,34 +14,32 @@ const getBrowserLanguage = () => {
   return ["en", "ar", "fr"].includes(baseLang) ? baseLang : "en";
 };
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        landing: enTranslations,
-      },
-      ar: {
-        landing: arTranslations,
-      },
-      fr: {
-        landing: frTranslations,
-      },
+const initOptions: InitOptions = {
+  resources: {
+    en: {
+      landing: enTranslations,
     },
-    fallbackLng: "en",
-    ns: ["landing"],
-    defaultNS: "landing",
-    interpolation: {
-      escapeValue: false,
+    ar: {
+      landing: arTranslations,
     },
-    detection: {
-      order: ["navigator"],
-      lookupNavigator: true,
-      caches: [],
+    fr: {
+      landing: frTranslations,
     },
-    lng: getBrowserLanguage(), // Set initial language
-  });
+  },
+  fallbackLng: "en",
+  ns: ["landing"],
+  defaultNS: "landing",
+  interpolation: {
+    escapeValue: false,
+  },
+  detection: {
+    order: ["localStorage", "navigator"],
+    caches: ["localStorage"],
+  },
+  lng: getBrowserLanguage(),
+};
+
+i18n.use(LanguageDetector).use(initReactI18next).init(initOptions);
 
 // Set initial RTL
 document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
